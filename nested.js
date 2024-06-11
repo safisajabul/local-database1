8e4609760b308f94e8d490a1d0db1531f338b56d
sajabul safi <155475466+safisajabul@users.noreply.github.com>
2024-06-11 17:30:36 +0545

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,21 @@
+let age1=15;
+let age2=17;
+let age3=18;
+
+if(age1>age2){
+    if(age1>age3){
+        document.write("age1 is greater");
+
+    }
+    else{
+        document.write("age3 is greater");
+    }
+}
+else{
+    if(age2>age3){
+        document.write("age2 is greater");
+    }
+    else{
+        document.write("age3 is greater");
+    }
+}
